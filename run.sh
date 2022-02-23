a97f0403d8496e0e6d7a8b1895822da39869b25e
hdmjdp <hdmjdp@163.com>
2022-02-23 16:46:03 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-python ./trainSpeakerNet.py --model ResNetSE34V2 --log_input True --nClasses 17121 --encoder_type ASP --n_mels 64 --trainfunc softmaxproto --save_path exps/exp2 --nPerSpeaker 2 --batch_size 200 --train_list /data/juicefs_speech_tts_v3/public_data/11139034/vp_corpus/train.list --train_path ''  --test_list /data/juicefs_speech_tts_v3/public_data/11139034/vp_corpus/test.list --test_path ''
+python ./trainSpeakerNet.py --model ResNetSE34V2 --log_input True --nClasses 17121 --encoder_type ASP --n_mels 3 --trainfunc softmaxproto --save_path exps/exp2 --nPerSpeaker 2 --batch_size 200 --train_list /data/juicefs_speech_tts_v3/public_data/11139034/vp_corpus/train.list --train_path ''  --test_list /data/juicefs_speech_tts_v3/public_data/11139034/vp_corpus/test.list --test_path ''
 
